5d90513647675bfdfd5b59d3ec3432dff86ee2a7
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-09-05 23:20:18 -0300

    acessa as informacoes e acessorios do carro

--- alura_motors.py
+++ alura_motors.py
@@ -6,14 +6,26 @@ url = 'https://alura-site-scraping.herokuapp.com/index.php'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0'}
 
 
+cards = []
+card = {}
+acessorios = []
+
 try:
     req = Request(url, headers=headers)
     response = urlopen(req)
     html = response.read()
     soup = BeautifulSoup(html, 'html.parser')
-    nome_carro = soup.find('p', {'class': 'txt-name'}).get_text().title()
-    valor_carro = soup.find('p', {'class': 'txt-value'}).get_text()
-    print('{} = {}'.format(nome_carro, valor_carro))
+    infos_carro = soup.find('div', {'class': 'body-card'}).find_all('p')
+    acessorios_carro = soup.find('div', {'class': 'body-card'}).find_all('li')
+    acessorios_carro.pop()
+
+    for info in infos_carro:
+        card[info.get('class')[0].split('-')[-1]] = info.get_text()
+
+    for itens in acessorios_carro:
+        card['itens'] = acessorios.append(itens.get_text().replace('►', ''))
+
+    print(card)
 
 except HTTPError as e:
     print(e.status, e.reason)
