f704edf0e635c12b2669649e5938ce9f374b2dbc
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-09-05 23:51:17 -0300

    criacao do dataframe com infos do primeiro carro

--- alura_motors.py
+++ alura_motors.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen, Request
 from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
+import pandas as pd
 
 url = 'https://alura-site-scraping.herokuapp.com/index.php'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0'}
@@ -20,15 +21,19 @@ try:
     acessorios_carro.pop()
 
     for info in infos_carro:
-        card[info.get('class')[0].split('-')[-1]] = info.get_text()
+        card[info.get('class')[0].split('-')[-1]] = (info.get_text()).title()
 
     for itens in acessorios_carro:
-        card['itens'] = acessorios.append(itens.get_text().replace('►', ''))
-
-    print(card)
-
+        acessorios.append(itens.get_text().replace('►', '').title())
+        card['items'] = acessorios
+ 
 except HTTPError as e:
     print(e.status, e.reason)
 
 except URLError as e:
     print(e.reason)
+
+
+df_carros = pd.DataFrame.from_dict(card, orient='index').transpose()
+
+df_carros.to_csv('dados/alura_motors.csv', index=False, encoding='utf-8-sig')
